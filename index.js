93b0ad27c6ca88108c47953928b2a4b46f9df73d
Harsh Dhobe <harshdhobe123@gmail.com>
2024-12-28 22:57:43 +0530

    mongodb connected

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const path = require("path");
-
-
 const app = express();
 const PORT = 8000;
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost:27017/blogify').then((e) => console.log('mongodb connected'));
 
 const userRoutes = require("./routes/user");
 
