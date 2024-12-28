b25abd04f65ce4594708a737d3371cd9137b5a86
Harsh Dhobe <harshdhobe123@gmail.com>
2024-12-28 14:21:42 +0530

    add presave in usermodel

--- models/user.js
+++ models/user.js
@@ -28,5 +28,22 @@ const userSchema = new schema(
 
 );
 
+//pre save in userSchema
+userSchema.pre("save", function (next) {
+    const user = this;
+
+    if (!user.isModified("password")) return;
+
+    const salt = randomBytes(16).toString();
+    const hashedPassword = createHmac("sha256", salt)
+        .update(user.password)
+        .digest("hex");
+
+    this.salt = salt;
+    this.password = hashedPassword;
+
+    next();
+});
+
 const User = model("user", userSchema);
 module.exports = User;
