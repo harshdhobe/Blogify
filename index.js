1ad164a6052d180d718d226ac880761bdab65774
Harsh Dhobe <harshdhobe123@gmail.com>
2024-12-28 13:35:20 +0530

    updates

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const PORT = 8000;
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server started on PORT ${PORT}`)
